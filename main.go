3c3bc5858208120b0589a5e8cf6ffcc1b19b422a
yu-fukunaga <92198819+yu-fukunaga@users.noreply.github.com>
2023-01-05 14:53:46 +0900

    Add server start command

---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "practice-server/cmd"
+
+func main() {
+	cmd.Execute()
+}
--- server.go
+++ server.go
@@ -1,29 +1,29 @@
 package main
 
-import (
-	"log"
-	"net/http"
-	"os"
-	"practice-server/graph"
-	"practice-server/graph/generated"
+// import (
+// 	"log"
+// 	"net/http"
+// 	"os"
+// 	"practice-server/graph"
+// 	"practice-server/graph/generated"
 
-	"github.com/99designs/gqlgen/graphql/handler"
-	"github.com/99designs/gqlgen/graphql/playground"
-)
+// 	"github.com/99designs/gqlgen/graphql/handler"
+// 	"github.com/99designs/gqlgen/graphql/playground"
+// )
 
-const defaultPort = "8080"
+// const defaultPort = "8080"
 
-func main() {
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = defaultPort
-	}
+// func main() {
+// 	port := os.Getenv("PORT")
+// 	if port == "" {
+// 		port = defaultPort
+// 	}
 
-	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
+// 	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
 
-	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
-	http.Handle("/query", srv)
+// 	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
+// 	http.Handle("/query", srv)
 
-	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
-	log.Fatal(http.ListenAndServe(":"+port, nil))
-}
+// 	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
+// 	log.Fatal(http.ListenAndServe(":"+port, nil))
+// }
